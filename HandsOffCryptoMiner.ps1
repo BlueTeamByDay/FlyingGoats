70607c38b98d9b98c12b58218bc6ea3a6ba97589
BlueTeamByDay <56196267+BlueTeamByDay@users.noreply.github.com>
2020-03-11 13:34:37 -0400

    Update HandsOffCryptoMiner.ps1

--- HandsOffCryptoMiner.ps1
+++ HandsOffCryptoMiner.ps1
@@ -1,6 +1,6 @@
 #
 # The sole purpose is to test Cuckoo Sandbox and make sure we can launch a GUI-less crypto mining activity 
-# and analyze it's traffic patter for the AI. 
+# and analyze it's traffic patter for the AI. The tool is preconfigured to use only 2 cores and mine monero. 
 #
 
 Invoke-WebRequest -Uri "https://github.com/BlueTeamByDay/RFDIR_Tools/raw/master/minertest.zip" -OutFile "C:\minertest.zip"
