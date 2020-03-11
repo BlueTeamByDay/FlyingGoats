9e84f16ee21d1521bd84b1557197dbab93ea3e9f
BlueTeamByDay <56196267+BlueTeamByDay@users.noreply.github.com>
2020-03-11 13:30:58 -0400

    Update HandsOffCryptoMiner.ps1

--- HandsOffCryptoMiner.ps1
+++ HandsOffCryptoMiner.ps1
@@ -1,3 +1,8 @@
+#
+# The sole purpose is to test Cuckoo Sandbox and make sure we can launch a GUI-less crypto mining activity 
+# and analyze it's traffic patter for the AI. 
+#
+
 Invoke-WebRequest -Uri "https://github.com/BlueTeamByDay/RFDIR_Tools/raw/master/minertest.zip" -OutFile "C:\minertest.zip"
 Expand-Archive -Path 'C:\minertest.zip' -DestinationPath '.\MinerTest'
 cd 'C:\MinerTest\xmr-stak-rx\'
